2879860c88e8358e81b520d93f402f78eb8cd3cf
andromeda <andromeda@Andromeda.local>
2022-06-15 11:13:26 -0400

    Commit finished repo

---
+++ near-marketplace/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useCallback, useState } from "react";
+import { Container, Nav } from "react-bootstrap";
+import { login, logout as destroy, accountBalance } from "./utils/near";
+import Wallet from "./components/Wallet";
+import { Notification } from "./components/utils/Notifications";
+import Products from "./components/marketplace/Products";
+import Cover from "./components/utils/Cover";
+import coverImg from "./assets/img/sandwich.jpg";
+import "./App.css";
+
+const App = function AppWrapper() {
+  const account = window.walletConnection.account();
+  const [balance, setBalance] = useState("0");
+  const getBalance = useCallback(async () => {
+    if (account.accountId) {
+      setBalance(await accountBalance());
+    }
+  });
+
+  useEffect(() => {
+    getBalance();
+  }, [getBalance]);
+  return (
+    <>
+      <Notification />
+      {account.accountId ? (
+        <Container fluid="md">
+          <Nav className="justify-content-end pt-3 pb-5">
+            <Nav.Item>
+              <Wallet
+                address={account.accountId}
+                amount={balance}
+                symbol="NEAR"
+                destroy={destroy}
+              />
+            </Nav.Item>
+          </Nav>
+          <main><Products /></main>
+        </Container>
+      ) : (
+        <Cover name="Street Food" login={login} coverImg={coverImg} />
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ near-marketplace/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { initializeContract } from "./utils/near";
+
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+window.nearInitPromise = initializeContract()
+  .then(() => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+  })
+  .catch(console.error);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
